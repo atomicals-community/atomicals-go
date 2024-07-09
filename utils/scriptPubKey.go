608cd408c7bc0e65bcd76575ae190b2fbef21116
yiming <isyiming.zhang@icloud.com>
2024-07-09 23:31:23 +0800

    add payment

--- utils/scriptPubKey.go
+++ utils/scriptPubKey.go
@@ -1,6 +1,9 @@
 package utils
 
-import "bytes"
+import (
+	"bytes"
+	"encoding/hex"
+)
 
 // is_unspendable_legacy
 func IsUnspendableLegacy(script []byte) bool {
@@ -12,4 +15,65 @@ func IsUnspendableLegacy(script []byte) bool {
 func IsUnspendableGenesis(script []byte) bool {
 	// OP_FALSE OP_RETURN
 	return bytes.Equal(script[:2], []byte{0x00, 0x6a})
+}
+
+func Is_op_return_subrealm_payment_marker_atomical_id(script []byte) string {
+	if len(script) < 1+5+2+1+36 { // 6a04<atom><01>p<atomical_id>
+		return ""
+	}
+	// Ensure it is an OP_RETURN
+	firstByte := script[0]
+	secondBytes := script[:2]
+	if secondBytes[0] != 0x00 && firstByte != 0x6a {
+		return ""
+	}
+	startIndex := 1
+	if secondBytes[0] == 0x00 {
+		startIndex = 2
+	}
+	// Check for the envelope format
+	if hex.EncodeToString(script[startIndex:startIndex+5]) != ATOMICALS_ENVELOPE_MARKER_BYTES {
+		return ""
+	}
+	// Check the next op code matches 'p' for payment
+	if hex.EncodeToString(script[startIndex+5:startIndex+5+2]) != "0170" {
+		return ""
+	}
+	// Check there is a 36 byte push data
+	if hex.EncodeToString(script[startIndex+5+2:startIndex+5+2+1]) != "24" {
+		return ""
+	}
+	// Extract and return the atomical ID
+	atomicalID := script[startIndex+5+2+1 : startIndex+5+2+1+36]
+	return hex.EncodeToString(atomicalID)
+}
+func Is_op_return_dmitem_payment_marker_atomical_id(script []byte) string {
+	if len(script) < 1+5+2+1+36 { // 6a04<atom><01>p<atomical_id>
+		return ""
+	}
+	// Ensure it is an OP_RETURN
+	firstByte := script[0]
+	secondBytes := script[:2]
+	if secondBytes[0] != 0x00 && firstByte != 0x6a {
+		return ""
+	}
+	startIndex := 1
+	if secondBytes[0] == 0x00 {
+		startIndex = 2
+	}
+	// Check for the envelope format
+	if hex.EncodeToString(script[startIndex:startIndex+5]) != ATOMICALS_ENVELOPE_MARKER_BYTES {
+		return ""
+	}
+	// Check the next op code matches 'p' for payment
+	if hex.EncodeToString(script[startIndex+5:startIndex+5+2]) != "0164" {
+		return ""
+	}
+	// Check there is a 36 byte push data
+	if hex.EncodeToString(script[startIndex+5+2:startIndex+5+2+1]) != "24" {
+		return ""
+	}
+	// Extract and return the atomical ID
+	atomicalID := script[startIndex+5+2+1 : startIndex+5+2+1+36]
+	return hex.EncodeToString(atomicalID)
 }
