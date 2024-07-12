0cb80eb8da33e7519772e60d4a4ad91ff16d9b15
yiming <isyiming.zhang@icloud.com>
2024-07-12 09:41:30 +0800

    add json and init

---
+++ repo/postsql/init/init.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/atomicals-go/pkg/conf"
+	"github.com/atomicals-go/repo/postsql"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitModels(db *gorm.DB) {
+	(&postsql.GlobalDirectFt{}).Init(db)
+	(&postsql.GlobalDistributedFt{}).Init(db)
+	(&postsql.UTXOFtInfo{}).Init(db)
+	(&postsql.UTXONftInfo{}).Init(db)
+	(&postsql.ModInfo{}).Init(db)
+	(&postsql.BtcTx{}).Init(db)
+	(&postsql.Location{}).Init(db)
+	(&postsql.BloomFilter{}).Init(db)
+}
+
+func AutoMigrate(db *gorm.DB) {
+	(&postsql.GlobalDirectFt{}).AutoMigrate(db)
+	(&postsql.GlobalDistributedFt{}).AutoMigrate(db)
+	(&postsql.UTXOFtInfo{}).AutoMigrate(db)
+	(&postsql.UTXONftInfo{}).AutoMigrate(db)
+	(&postsql.ModInfo{}).AutoMigrate(db)
+	(&postsql.BtcTx{}).AutoMigrate(db)
+	(&postsql.Location{}).AutoMigrate(db)
+	(&postsql.BloomFilter{}).AutoMigrate(db)
+}
+
+func main() {
+	conf, err := conf.ReadJSONFromJSFile("./config.json")
+	if err != nil {
+		panic(err)
+	}
+	DB, err := gorm.Open(postgres.Open(conf.SqlDNS), &gorm.Config{DisableForeignKeyConstraintWhenMigrating: true})
+	if err != nil {
+		panic(err)
+	}
+	InitModels(DB)
+	// AutoMigrate(DB)
+}
